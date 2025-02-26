fc8e17edb5b3587a0b2c67e2a3b474449c0e8a3e
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2025-02-26 20:10:22 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict"
+function layer(a,b,c)
+{
+	c = Math.floor(c)
+	console.log("layer", [a,b,c])
+	let layers = "eFGH"
+	let E = ""
+	if (c === 1)
+	{
+		if (b < 6)
+		{
+			E = a * (10**b)
+		}
+		else
+		{
+			E = a.toFixed(6) + "e" + b
+		}
+	}
+	else if (c > 1)
+	{
+		if (b < 5)
+		{
+			E = ((c < 4) ? layers[c-2] : ("[" + c + "]")).repeat(b-1) + layer(10**(a%1),Math.floor(a),c-1)
+		}
+		else
+		{
+			E = a.toFixed(6) + ((c < 4) ? layers[c-1] : ("[" + c + "]")) + b
+		}
+	}
+	return E
+}
