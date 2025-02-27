fb728b5102bdc0ac74a561089eaa044f51d5a3a3
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2025-02-27 12:44:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -53,6 +53,6 @@ setInterval(function()
 		y += 1;
 		x = (x%10)+1
 	}
-	text = layer(x,(y%9)+1,Math.floor(y/9)+1)
+	text = layer(x,(y%8)+1,Math.floor(y/8)+1)
 	document.getElementById("text").innerHTML = text
 }, 30)
